d0fef1adf79088f5721886ab0b7dfa8ebd5b6bcb
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-04-23 15:37:48 +0200

    uzycie auth store

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="flex items-center justify-center h-full">
-    <h2 class="text-[2.5rem] font-bold text-[#1a3b22] tracking-tight">Good morning, Warden.</h2>
+    <h2 
+    v-if="auth.isLoggedIn"
+    class="text-[2.5rem] font-bold text-[#1a3b22] tracking-tight">Good morning, {{ auth.userEmail }}!</h2>
   </div>
 </template>
 
 <script setup>
+import { useAuthStore } from '../stores/auth';
+
+const auth = useAuthStore();
 </script>
