bdbf362f2dcdcdf40111441a25aa31a5d206042b
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-04-24 11:56:17 +0200

    dodano sprawdzanie roli

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,22 +1,35 @@
 import { defineStore } from 'pinia';
+import { jwtDecode } from 'jwt-decode';
 import { computed, ref } from 'vue';
 import AuthService from '../services/auth.service';
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref(localStorage.getItem('jwt') ?? null);
   const userEmail = ref(localStorage.getItem('user_email') ?? null);
+  const user = ref(null)
 
   const isLoggedIn = computed(() => !!token.value);
+  const hasRole = (role) => user.value?.role === role;
   
   async function login(email, password) {
     const data = await AuthService.login(email, password);
     token.value = data.token ?? null;
     userEmail.value = data.email ?? email;
+    setUser()
     return data;
   }
 
   async function register(firstName, lastName, birthDate, email, phone, password) {
     return await AuthService.register(firstName, lastName, birthDate, email, phone, password);
+  }
+
+  function setUser(){
+    if(token.value){
+      const decoded = jwtDecode(token.value)
+      user.value = {
+        role: decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+      }
+    }
   }
    
   function logout() {
@@ -29,6 +42,8 @@ export const useAuthStore = defineStore('auth', () => {
     token,
     userEmail,
     isLoggedIn,
+    user,
+    hasRole,
     login,
     register,
     logout,
