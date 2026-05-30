2117214eb1782456d5738840d39d38aacb525b66
unknown <miczejko.kacper@gmail.com>
2026-05-30 16:19:56 +0200

    changed notification structure and deleted broken popup

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,17 +1,26 @@
 import axios from "axios";
+import * as signalR from "@microsoft/signalr";
+
 
 const URL = "https://localhost:7293/Account/";
 
 class AuthService {
 
     async login(email, password) {
-        const response = await axios.post(URL + "Login", { email, password });
-        if (response.data.token) {
-            localStorage.setItem("jwt", response.data.token);
-            localStorage.setItem("user_email", response.data.email);
-        }        
-        return response.data;
-    
+        try {
+            const response = await axios.post(URL + "Login", { email, password });
+            if (response.data.token) {
+                localStorage.setItem("jwt", response.data.token);
+                localStorage.setItem("user_email", response.data.email);
+                console.log('response.data:', response.data);
+                console.log('response.data.token type:', typeof response.data.token);
+                console.log('response.data.token value:', response.data.token);
+            }
+            return response.data;
+        } catch (error) {
+            console.error('Login failed:', error);
+            throw error;
+        }
     }
 
     async register(firstName, lastName, birthDate, email, phone, password) {
